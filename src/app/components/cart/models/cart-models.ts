d471e857a2a15941048e1b580b2f7ea80d337236
Pavel Karenda <korendos@gmail.com>
2021-02-24 17:33:17 +0300

    feat: add new cart model

--- src/app/components/cart/models/cart-models.ts
+++ src/app/components/cart/models/cart-models.ts
@@ -1,4 +1,10 @@
+import {BookModel} from '../../book/models/book-model';
+
 export interface CartItem {
   id: number;
   count: number;
+}
+
+export interface CartBook extends BookModel {
+  bookCount: number;
 }
