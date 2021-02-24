56c08d2e95610a690ac955be2643be7cf6b0831d
Pavel Karenda <korendos@gmail.com>
2021-02-24 17:33:44 +0300

    feat: init start methods of cart service

--- src/app/components/cart/cart.service.ts
+++ src/app/components/cart/cart.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {CartItem} from './models/cart-models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  // cart storage
+  cartProduct: CartItem[] = [];
+
+  // cart items quantity
+  totalQuantity: number;
+
+  // total cart cost
+  totalSum: number;
+
   constructor() { }
+
+  getAllItemsInCart(): CartItem[] {
+    return this.cartProduct;
+  }
+
+  addBook(id): void {}
+  removeBook(): void {}
+  increaseQuantity(): void {}
+  decreaseQuantity(): void {}
+  removeAllBooks(): void {}
+
+  // recalculate total quantity and cart cost after each action
+  updateCartData(): void {}
 }
