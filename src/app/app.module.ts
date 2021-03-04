ebe0f675425ebe9208690354c404c746cfec7845
Pavel Karenda <korendos@gmail.com>
2021-03-04 20:51:01 +0300

    feat: add services into providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { FormsModule } from '@angular/forms';
 import { BookListComponent } from './components/book/components/book-list/book-list.component';
 import { CartListComponent } from './components/cart/components/cart-list/cart-list.component';
 import { BookItemComponent } from './components/book/components/book-item/book-item.component';
+import {LocalStorageService} from './core/services/local-storage.service';
+import {ConstantsService} from './core/services/constants.service';
+import {ConfigOptionsService} from './core/services/config-options.service';
+import {GeneratorFactory, GeneratorService} from './core/services/generator.service';
+import { AboutComponent } from './layout/components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +19,19 @@ import { BookItemComponent } from './components/book/components/book-item/book-i
     CartItemComponent,
     BookListComponent,
     CartListComponent,
-    BookItemComponent
+    BookItemComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LocalStorageService, useClass: LocalStorageService},
+    {provide: ConstantsService, useValue: ConstantsService},
+    ConfigOptionsService,
+    {provide: GeneratorService, useFactory: GeneratorFactory(3), deps: [GeneratorService]}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
