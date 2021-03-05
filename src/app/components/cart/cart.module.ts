b8f01406472694c4c3cfb1897ca9220db20de969
Pavel Karenda <korendos@gmail.com>
2021-03-05 11:11:45 +0300

    feat: add cart component init version

--- src/app/components/cart/cart.module.ts
+++ src/app/components/cart/cart.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
 import {CartListComponent} from './components/cart-list/cart-list.component';
 import {CartItemComponent} from './components/cart-item/cart-item.component';
+import { CartComponent } from './cart.component';
 
 @NgModule({
   imports: [
@@ -10,6 +11,7 @@ import {CartItemComponent} from './components/cart-item/cart-item.component';
   declarations: [
     CartListComponent,
     CartItemComponent,
+    CartComponent,
   ],
 })
 export class CartModule {}
