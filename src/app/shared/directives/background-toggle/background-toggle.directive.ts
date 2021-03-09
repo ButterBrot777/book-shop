022091745de6e81a1c748e3a253654f5a55c2c46
Pavel Karenda <korendos@gmail.com>
2021-03-09 13:25:47 +0300

    refactor: refactor according code review

--- src/app/shared/directives/background-toggle/background-toggle.directive.ts
+++ src/app/shared/directives/background-toggle/background-toggle.directive.ts
@@ -1,20 +1,11 @@
-import {Directive, ElementRef, Renderer2, HostListener, HostBinding} from '@angular/core';
+import {Directive, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appBGToggle]'
 })
 export class BackgroundToggleDirective {
-  constructor(private element: ElementRef, private renderer: Renderer2){
-
-    this.renderer.setStyle(this.element.nativeElement, 'cursor', 'pointer');
-  }
 
   @HostBinding('style.backgroundColor') backgroundColor: string;
-
-  // this way is the same as in constructor via renderer to set cursor pointer
-  // @HostBinding('style.cursor') get getCursor(): string {
-  //   return 'pointer';
-  // }
 
   @HostListener('mouseenter') onMouseEnter(): void {
     this.backgroundColor = '#f3eded';
--- src/app/shared/directives/highlight-on-click/highlight-on-click.directive.ts
+++ src/app/shared/directives/highlight-on-click/highlight-on-click.directive.ts
@@ -1,16 +1,12 @@
-import {AfterViewInit, Directive, ElementRef, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appHighlightOnClick]',
-  // tslint:disable-next-line:no-host-metadata-property
-  host: {
-    '(click)': 'toggleHighlight()',
-  }
 })
-export class HighlightOnClickDirective implements AfterViewInit{
-  @Input() backgroundColor = '#28a745';
-  @Input() borderWidth = '2px';
-  @Input() borderColor = 'yellow';
+export class HighlightOnClickDirective implements OnInit {
+  @Input() backgroundColor: string;
+  @Input() borderWidth: string;
+  @Input() borderColor: string;
 
   private isInitialState = true;
 
@@ -22,18 +18,22 @@ export class HighlightOnClickDirective implements AfterViewInit{
   constructor(
     private elementRef: ElementRef<HTMLElement>,
     private renderer: Renderer2,
-  ) { }
+  ) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'cursor', 'pointer');
+  }
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     this.initialBackgroundColor = this.elementRef.nativeElement.style.backgroundColor;
     this.initialBorderColor = this.elementRef.nativeElement.style.borderColor;
     this.initialBorderWidth = this.elementRef.nativeElement.style.borderWidth;
     this.initialBorderStyle = this.elementRef.nativeElement.style.borderStyle;
   }
 
-  toggleHighlight(): void {
-    this.isInitialState = !this.isInitialState;
+  @HostListener('click') onClick(): void {
+    this.toggleHighlight();
+  }
 
+  private changeBackgroundColor(): void {
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       'backgroundColor',
@@ -41,23 +41,38 @@ export class HighlightOnClickDirective implements AfterViewInit{
         ? this.backgroundColor
         : this.initialBackgroundColor,
     );
+  }
 
+  private changeOutlineStyle(): void {
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       'outlineStyle',
       this.isInitialState ? 'solid' : this.initialBorderStyle,
     );
+  }
 
+  private changeOutlineWidth(): void {
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       'outlineWidth',
       this.isInitialState ? this.borderWidth : this.initialBorderWidth,
     );
+  }
 
+  private changeOutlineColor(): void {
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       'outlineColor',
       this.isInitialState ? this.borderColor : this.initialBorderColor,
     );
+  }
+
+  toggleHighlight(): void {
+    this.changeBackgroundColor();
+    this.changeOutlineStyle();
+    this.changeOutlineWidth();
+    this.changeOutlineColor();
+
+    this.isInitialState = !this.isInitialState;
   }
 }
