6fb6db25b7fb04ad6cc561767ede2ae26086f977
Pavel Karenda <korendos@gmail.com>
2021-03-14 19:30:50 +0300

    refactor: change detection strategy

--- src/app/book-page/components/book-item/book-item.component.ts
+++ src/app/book-page/components/book-item/book-item.component.ts
@@ -1,26 +1,20 @@
-import {Component, Input, OnInit, EventEmitter, Output, OnDestroy} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 
 import {Book} from '../../models/book-model';
 
 @Component({
   selector: 'app-book-item',
   templateUrl: './book-item.component.html',
-  styleUrls: ['./book-item.component.scss']
+  styleUrls: ['./book-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BookItemComponent implements OnInit, OnDestroy {
+export class BookItemComponent {
   @Input() book: Book;
   @Output() add = new EventEmitter<Book>();
 
   isPrimary = true;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
-    console.log('Book was destroyed');
-  }
 
   dateToString(time: number): Date {
     return new Date(time);
